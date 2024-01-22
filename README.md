9aaf1036e3644f99e7a1d0de3b1b57d7d03fce41
Josh <joshuamace@gmail.com>
2024-01-22 12:48:34 -0600

    added files from pt1 of project

---
+++ README.md
@@ -0,0 +1,10 @@
+# personal-website-pt1
+This is the personal website of Joshua Mace. It features the following
+pages in addition there are various personal photos;
+
+-Home Page
+-About Me
+-Portfolio
+
+Credit to a tutorial from Kevin Powell in creating popout image on the
+home page.
---
+++ script.js
@@ -0,0 +1,37 @@
+// start photo carousel
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+const popperson = document.querySelector('.person');
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
+slider.addEventListener('dblclick', e => {
+    alert("Don't double click! This section scrolls side to side!")
+  });
+  
+popperson.addEventListener('dblclick', e => {
+    alert("Don't double click! This section scrolls side to side!")
+  });
